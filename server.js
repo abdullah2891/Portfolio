965ad85bab2828c6de19f6d7ea86f1c48cad54fd
Abdullah  Rahman <abdullahrahmn@Abdullahs-MacBook-Pro.local>
2018-10-28 20:05:10 -0400

    Squashed commit of the following:
    commit e57043bd23c079636da4f7f618308ea1b75cb422
    Author: Abdullah  Rahman <abdullahrahmn@Abdullahs-MacBook-Pro.local>
    Date:   Sun Oct 28 19:58:40 2018 -0400
    
        node server

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.static('dist/'));
+
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.listen(PORT,function(){
+	console.log("Server Started");
+});
