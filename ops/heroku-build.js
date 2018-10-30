0a4851acdf091e7607d9fb99ea10832dbf312cd8
Abdullah  Rahman <abdullahrahmn@Abdullahs-MacBook-Pro.local>
2018-10-30 18:41:09 -0400

    please work

--- ops/heroku-build.js
+++ ops/heroku-build.js
@@ -21,7 +21,10 @@ if ('HEROKU' in process.env || ('DYNO' in process.env && process.env.HOME === '/
 
     console.time("build");
     console.log("starting npm build");
-    ChildProcess.execSync(`npm run build`);
+    ChildProcess.execSync(`npm run build`,function(error){
+    	console.warn(error);
+    
+    });
     console.timeEnd("build");
 
     console.time("uninstall");
